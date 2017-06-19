4e167bf8acb7dcf30697aef837a8339b3d3702f8
Cedric de Villoutreys <cedricdevilloutreys@gmail.com>
2017-06-19 19:19:45 +0200

    cloudinary installed

--- Gemfile
+++ Gemfile
@@ -1,14 +1,19 @@
 source 'https://rubygems.org'
 ruby '2.3.3'
 
-gem 'devise'
+gem 'rails', '5.0.3'
+
 gem 'figaro'
 gem 'jbuilder', '~> 2.0'
 gem 'pg'
 gem 'puma'
-gem 'rails', '5.0.3'
 gem 'redis'
 gem 'devise'
+
+gem 'cloudinary', '1.1.7'
+gem 'attachinary', github: 'assembler/attachinary'
+gem 'jquery-fileupload-rails'
+gem 'coffee-rails'
 
 gem 'autoprefixer-rails'
 gem 'bootstrap-sass'
@@ -24,6 +29,4 @@ group :development, :test do
   gem 'listen', '~> 3.0.5'
   gem 'pry-byebug'
   gem 'pry-rails'
-  gem 'spring'
-  gem 'spring-watcher-listen', '~> 2.0.0'
 end
--- config/application.rb
+++ config/application.rb
@@ -10,21 +10,11 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require "attachinary/orm/active_record"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module GALLERI
+module GallerI
   class Application < Rails::Application
-    config.generators do |generate|
-      generate.assets false
-      generate.helper false
-    end
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
   end
 end
