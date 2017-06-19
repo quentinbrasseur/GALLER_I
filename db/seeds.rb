e45abf81e0e2cb2572781c5afdb82912491717a3
quentinbrasseur <quentinbrasseur@msn.com>
2017-06-19 17:29:26 +0200

    schema and seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,56 +10,84 @@ p "seeding..."
 
 Artwork.destroy_all
 User.destroy_all
+
 quentin = User.new(
-  name: "quentin",
+  username: "quentin",
   email: "quentin@quentin.com",
   password: "password",
-  phone_number: "06 16 28 83 97")
+  phone_number: "06 16 28 83 97"
+  )
 quentin.save!
+
 kilian = User.new(
-  name: "kilian",
+  username: "kilian",
   email: "kilian@kilian.com",
   password: "password",
-  phone_number: "06 19 20 13 37")
+  phone_number: "06 19 20 13 37"
+  )
 kilian.save!
+
 cedric = User.new(
-  name: "cedric",
+  username: "cedric",
   email: "cedric@cedric.com",
   password: "password",
-  phone_number: "06 10 22 93 90")
+  phone_number: "06 10 22 93 90"
+  )
 cedric.save!
+
 joe = User.new(
-  name: "joe",
+  username: "joe",
   email: "joe@joe.com",
   password: "password",
-  phone_number: "06 06 20 81 37")
+  phone_number: "06 06 20 81 37"
+  )
 joe.save!
+
+
 mona_lisa = Artwork.new(
   title: 'Mona Lisa',
-  category: 'Old school',
-  size: "50x50",
-  location: "Gracia",
-  user_id: quentin.id)
+  category: 'Painting',
+  height: 100,
+  width: 300,
+  depth: 20,
+  weight: 5,
+  location: "Madrid",
+  owner: quentin
+  )
+
 king_kong = Artwork.new(
   title: 'King Kong',
-  category: 'Pop Art',
-  size: "100x50",
-  location: "Sagrada",
-  user_id: cedric.id)
+  category: 'Painting',
+  height: 100,
+  width: 200,
+  depth: 5,
+  weight: 9,
+  location: "Sevilla",
+  owner: joe
+  )
+
 barcelona = Artwork.new(
-  title: 'Barcelona',
-  category: 'Landscape',
-  size: "200x200",
-  location: "Poblenou",
-  user_id: kilian.id
+  title: 'Cityscapper',
+  category: 'Photography',
+  height: 100,
+  width: 100,
+  depth: 9,
+  weight: 2,
+  location: "Barcelona",
+  owner: cedric
   )
+
 fruits = Artwork.new(
-  title: 'Fruits',
+  title: 'Sculpture',
   category: 'Nature morte',
-  size: "20x20",
-  location: "Raval",
-  user_id: joe.id
+  height: 100,
+  width: 360,
+  depth: 2,
+  weight: 3,
+  location: "Bilbao",
+  owner: kilian
   )
+
 mona_lisa.save!
 barcelona.save!
 fruits.save!
