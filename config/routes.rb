a397e64705a9d8f2e4697cfa5814b58fcd9f7a2d
Cedric de Villoutreys <cedricdevilloutreys@gmail.com>
2017-06-20 12:21:32 +0200

    controller venues finished

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  resources :venues, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  resources :venues
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
