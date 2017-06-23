0471a7350c44adc26a634ed7133ca315dbc0f21c
quentinbrasseur <quentinbrasseur@msn.com>
2017-06-23 14:32:16 +0200

    wip

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -3,6 +3,9 @@ class Artwork < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key:'user_id'
   has_many :appointments, dependent: :destroy
   has_many :venues, through: :appointments
+
   geocoded_by :location
+  # after_validation :geocode
   after_validation :geocode, if: :location_changed?
+  # after_create :geocode
 end
