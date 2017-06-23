fa0a854dc9544865dd41eac1f4eea3641f20144d
Joseph DiFrancesco <joseph.difrancesco@gmail.com>
2017-06-23 15:33:48 +0200

    final push for heroku

--- app/assets/javascripts/datetimepicker.js
+++ app/assets/javascripts/datetimepicker.js
@@ -1,35 +1,49 @@
 $(document).ready(function(){
 
+ $('#start_date').datetimepicker({
 
+disabledDates: window.DISABLE_DATES,
+  });
+
+        $('#end_date').datetimepicker({
+            useCurrent: false, //Important! See issue #1075
+            disabledDates: window.DISABLE_DATES,
+        });
+
+        $("#start_date").on("dp.change", function (e) {
+
+
+            $('#end_date').data("DateTimePicker").minDate(e.date);
+
+        });
+        $("#end_date").on("dp.change", function (e) {
+
+            $('#start_date').data("DateTimePicker").maxDate(e.date);
+
+        });
 
 // $('#start_date').datetimepicker();
 
 
 
-// $('#start_date').datetimepicker();
-//         $('#end_date').datetimepicker({
-//             useCurrent: false //Important! See issue #1075
-//         });
-//         $("#start_date").on("dp.change", function (e) {
-//             $('#end_date').data("DateTimePicker").minDate(e.date);
-//         });
-//         $("#end_date").on("dp.change", function (e) {
-//             $('#start_date').data("DateTimePicker").maxDate(e.date);
-//         });
 
-$('#start_date').datetimepicker({
 
-                    disabledDates: window.DISABLE_DATES
+// $('#start_date').datetimepicker({
 
-                });
-$('#end_date').datetimepicker({
+//                     disabledDates: window.DISABLE_DATES,
+//                 //     icons: {
+//                 //     time: "fa fa-clock-o",
+//                 //     date: "fa fa-calendar",
+//                 //     up: "fa fa-arrow-up",
+//                 //     down: "fa fa-arrow-down"
+//                 // }
 
-                    disabledDates: window.DISABLE_DATES
+//                 });
+// $('#end_date').datetimepicker({
 
-                });
+//                     disabledDates: window.DISABLE_DATES,
 
-// $("#end_date").datepicker();
-// $("#start_request").datepicker();
-// $("#end_request").datepicker();
-//$(".datepicker").datepicker()
+//                 });
+
+
 });
