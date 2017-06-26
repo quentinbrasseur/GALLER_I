e9ff10c5ab5e2592d0b4ccd3e154f23065c0b524
Joseph DiFrancesco <joseph.difrancesco@gmail.com>
2017-06-26 09:12:27 +0200

    heroku push

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -9,7 +9,7 @@ def create
     recipients = User.where(id: conversation_params[:recipients])
     conversation = current_user.send_message(recipients, conversation_params[:body], conversation_params[:subject]).conversation
     flash[:success] = "Your message was successfully sent!"
-raise
+
     redirect_to profile_path(current_user)
 
   end
